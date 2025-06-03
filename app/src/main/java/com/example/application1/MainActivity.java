d0f24a8340a3a0967b6ed8e41b6648a57fe9ca88
tina0928391507 <881104tina@gmail.com>
2025-06-03 15:48:36 +0800

    6/3

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.application1;
 
 import android.os.Bundle;
-
+import android.widget.CheckedTextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,11 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_edittext);
+        setContentView(R.layout.activity_linearlayout);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
         //});
+       // mCheckedTextView01 = (CheckedTextView) findViewById(R.id.checkedTextView01);
+        //mCheckedTextView01.setChecked(ture);
+        //mCheckedTextView01.setChecked(ture);
     }
 }
